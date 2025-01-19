3e544c7e47815c731956794526af29c658d8fbb5
Melody Cartwright <melody94c@gmail.com>
2025-01-19 16:37:05 +0100

    Updated routes/users.js to add delete route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,10 @@
-const { getUsers, getUserById } = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
+const { getUsers, getUserById, deleteUser } = require("../controllers/userController");
+const { isAdmin } = require('../middleware/auth');
 
 router.get("/", getUsers);
 router.get("/:userId", getUserById);
+router.delete('/:userId', isAdmin, deleteUser);
 
 module.exports = router;
