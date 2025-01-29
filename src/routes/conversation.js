f017aee1ca9a215784500a738f19496d157941ed
Amanda Håkansson <amanda.deckeman@gmail.com>
2025-01-29 02:10:07 +0100

    added get conversation id

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const {createConversation, getConversation, inviteToConversation} = require('../controllers/conversationController')
-const {isLoggedIn} = require('../middleware/auth')
+const { createConversation, getConversation, inviteToConversation, } = require("../controllers/conversationController");
+const { isLoggedIn, verifyAuth } = require("../middleware/auth");
+const { Conversation } = require("../models/conversationModels");
 
-router.post('/', isLoggedIn, createConversation)
-router.get('/', isLoggedIn, getConversation)
-router.post('/:conversationId/invite/:userId', isLoggedIn, inviteToConversation)
+router.post("/", isLoggedIn, createConversation);
+
+router.get("/", isLoggedIn, getConversation);
+
+router.post("/:conversationId/invite/:userId", isLoggedIn, inviteToConversation);
+
+router.get("/conversations", verifyAuth, async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const conversations = await Conversation.find(
+      { participants: userId },
+      "_id"
+    );
+
+    const conversationIds = conversations.map((conversation) => conversation._id);
+
+    res.status(200).json({ success: true, conversationIds });
+  } catch (error) {
+    console.error("Fel vid hämtning av konversationer:", error);
+    res
+      .status(500)
+      .json({ error: "Ett fel uppstod vid hämtning av konversationer." });
+  }
+});
 
 module.exports = router;
